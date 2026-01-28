5f098e4c776734a66dc66eb53debe78c212614aa
InnaZephyr <innazakirova24@gmail.com>
2026-01-28 22:33:15 +0200

    complete assignment

--- src/ru/yandex/practicum/ParkingLot.java
+++ src/ru/yandex/practicum/ParkingLot.java
@@ -1,27 +1,75 @@
 package ru.yandex.practicum;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
 public class ParkingLot extends AbstractParkingLot {
 
+    private int freeN;
+    private int freeE;
+    private int freeP;
+    private final HashMap<String, SpotType> parked = new HashMap<>();
+
     public ParkingLot(int totalSpots, int electricSpots, int premiumSpots) {
         super(totalSpots, electricSpots, premiumSpots);
+        int normalSpots = totalSpots - electricSpots - premiumSpots;
+        this.freeN = normalSpots;
+        this.freeE = electricSpots;
+        this.freeP = premiumSpots;
+
+    }
+
+    @Override
+    boolean isEmpty() {
+        return parked.isEmpty();
     }
 
     @Override
     public void enter(String carType, String number) throws ParkingException {
-        //TODO
+          if (parked.containsKey(number)) {
+              throw new ParkingException("already parked");
+          }
+
+          if (carType.equals("NORMAL") && freeN > 0) {
+              freeN--;
+              parked.put(number, SpotType.NORMAL);
+          } else if (carType.equals("ELECTRIC") && freeE > 0) {
+              freeE--;
+              parked.put(number, SpotType.ELECTRIC);
+          } else if (carType.equals("PREMIUM") && freeP > 0) {
+              freeP--;
+              parked.put(number, SpotType.PREMIUM);
+          } else if (carType.equals("PREMIUM") && freeN > 0) {
+              freeN--;
+              parked.put(number, SpotType.NORMAL);
+          } else if (carType.equals("NORMAL") || carType.equals("ELECTRIC") || carType.equals("PREMIUM")) {
+            throw new ParkingException("no free parking spot");
+          } else {
+            throw new RuntimeException("unknown car type " + carType);
+          }
+
     }
 
     @Override
     public void leave(String number) throws ParkingException {
-        //TODO
+        SpotType spot = parked.get(number);
+        if (spot == null) throw new ParkingException("not parked");
+
+       if (spot == SpotType.NORMAL) {
+           freeN++;
+       } else if (spot == SpotType.ELECTRIC) {
+           freeE++;
+       } else if (spot == SpotType.PREMIUM) {
+           freeP++;
+       } else {
+           throw new RuntimeException("unknown spot type " + spot);
+       }
+        parked.remove(number);
     }
 
     @Override
     public Set<String> getNumbers() {
-        //TODO
-        return new HashSet<>();
+        return new HashSet<>(parked.keySet());
     }
 }
--- test/ru/yandex/practicum/ParkMateTest.java
+++ test/ru/yandex/practicum/ParkMateTest.java
@@ -1,7 +1,67 @@
 package ru.yandex.practicum;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ParkMateTest {
+
+
+    @Test
+    void normalCanEnterAndNumberAppearsInSet() throws Exception {
+        ParkingLot parkingLot = new ParkingLot(1, 0, 0);
+        parkingLot.enter("NORMAL", "A001AA");
+        assertTrue(parkingLot.getNumbers().contains("A001AA"));
+    }
+
+    @Test
+    void electricCantEnterWhenZeroElectricSpots() {
+        ParkingLot parkingLot = new ParkingLot(5, 0, 0);
+        assertThrows(ParkingException.class, () -> parkingLot.enter("ELECTRIC", "E001EE"));
+    }
+
+    @Test
+    void premiumTakesPremiumSpotIfExists() throws Exception {
+        ParkingLot parkingLot = new ParkingLot(2, 0, 1);
+        parkingLot.enter("PREMIUM", "P001PP");
+        parkingLot.enter("NORMAL", "N001NN");
+
+        assertTrue(parkingLot.getNumbers().contains("P001PP"));
+        assertTrue(parkingLot.getNumbers().contains("N001NN"));
+    }
+
+    @Test
+    void premiumTakesNormalSpotIfZeroPremiumSpots() throws Exception {
+        ParkingLot parkingLot = new ParkingLot(2, 0, 1);
+        parkingLot.enter("PREMIUM", "P001PP");
+        parkingLot.enter("PREMIUM", "P002PP");
+
+        assertThrows(ParkingException.class, () -> parkingLot.enter("NORMAL", "N001NN"));
+    }
+
+    @Test
+    void enteringAgainWithSameNumber() throws Exception {
+        ParkingLot parkingLot = new ParkingLot(1, 0, 0);
+        parkingLot.enter("NORMAL", "A001AA");
+
+        assertThrows(ParkingException.class, () -> parkingLot.enter("NORMAL", "A001AA"));
+    }
+
+    @Test
+    void leavingNotParkedCarThrows() {
+        ParkingLot parkingLot = new ParkingLot(1, 0, 0);
+        assertThrows(ParkingException.class, () -> parkingLot.leave("A001AA"));
+    }
+
+
+    @Test
+    void leaveFreesSpotSoAnotherCarCanEnter() throws Exception {
+        ParkingLot parkingLot = new ParkingLot(1, 0, 0);
+        parkingLot.enter("NORMAL", "A001AA");
+        parkingLot.leave("A001AA");
+        parkingLot.enter("NORMAL", "B002BB");
+
+        assertTrue(parkingLot.getNumbers().contains("B002BB"));
+    }
 
 }
